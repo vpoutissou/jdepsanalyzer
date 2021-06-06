b07f277638e34b99c0b153510789e1723f11d510
Vincent Poutissou <vincent.poutissou@gmail.com>
2021-06-06 17:05:30 +0200

    [feat] adding some javadoc to PackageRepresentation interface

--- src/main/java/com/jdepsanalyzer/PackageRepresentation.java
+++ src/main/java/com/jdepsanalyzer/PackageRepresentation.java
@@ -5,14 +5,23 @@ import java.util.Set;
 
 public interface PackageRepresentation {
 
+  /**
+   * @return the name of the package
+   */
   String getName();
 
+  /**
+   * @return the jar name
+   */
   String getJarName();
 
   Set<PackageRepresentation> getDependencies();
 
   boolean isOkForIsolation();
 
+  /**
+   * @return the first cycle found when checking for isolation
+   */
   List<PackageRepresentation> getCycle();
 
 }
